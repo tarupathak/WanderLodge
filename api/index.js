45c59105d985835ba54338d94b45a5b817aa2bd1
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-09-10 18:36:48 +0530

    Added hotel model and setup routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from './routes/auth.js'
+import userRoute from './routes/users.js'
+import hotelRoute from './routes/hotels.js'
+import roomRoute from './routes/rooms.js'
 const app = express();
 dotenv.config();
 
@@ -20,6 +24,15 @@ mongoose.connection.on("disconnected", () => {
 mongoose.connection.on("connected", () => {
   console.log("mongoDB connected");
 });
+
+//middlewares
+
+app.use(express.json())
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/hotels", hotelRoute);
+app.use("/api/v1/rooms", roomRoute);
+
 
 app.listen(5000, () => {
   dbConnect();
